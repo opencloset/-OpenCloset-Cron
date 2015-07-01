91d55b53c14b9c5b84040e8055299bd5d4307f74
Keedi Kim <keedi.k@gmail.com>
2015-07-01 17:17:39 +0900

    Ready to dzil

---
+++ lib/OpenCloset/Cron.pm
@@ -0,0 +1,24 @@
+package OpenCloset::Cron;
+# ABSTRACT: OpenCloset cron class
+
+use utf8;
+use strict;
+use warnings;
+
+our $VERSION = '0.001';
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=head1 SYNOPSIS
+
+    use OpenCloset::Cron;
+
+    ...
+
+=head1 DESCRIPTION
+
+...
