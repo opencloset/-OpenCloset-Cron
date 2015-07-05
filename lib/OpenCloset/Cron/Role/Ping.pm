c86bb2ea8a2507a432caa453cb493c9f8fd422f0
Keedi Kim <keedi.k@gmail.com>
2015-07-05 23:06:10 +0900

    Make it work :-)
    AnyEvent의 디버그 메시지 때문에 테스트에 실패하기 때문에 로그의
    경고 레벨을 debug 바로 윗 단계인 info로 변경합니다.
    
    AE_LOG=filter=info dzil test
    AE_LOG=filter=info dzil xtest

---
+++ lib/OpenCloset/Cron/Role/Ping.pm
@@ -0,0 +1,119 @@
+package OpenCloset::Cron::Role::Ping;
+# ABSTRACT: OpenCloset cron ping role
+
+use utf8;
+
+use Moo::Role;
+use MooX::Types::MooseLike::Base qw( CodeRef );
+use namespace::clean -except => 'meta';
+
+our $VERSION = '0.001';
+
+use AnyEvent::HTTPD;
+use AnyEvent;
+
+use OpenCloset::Patch::AnyEvent::HTTPD;
+use OpenCloset::Patch::Object::Event;
+
+requires 'httpd';
+
+has ping => (
+    is      => 'ro',
+    isa     => CodeRef,
+    builder => '_builder_ping',
+);
+
+sub _builder_ping {
+    return sub { return 1; };
+}
+
+after BUILD => sub {
+    my $self = shift;
+
+    $self->httpd->reg_cb(
+        '/ping' => sub {
+            my ( $httpd, $req ) = @_;
+
+            if ( $self->ping ) {
+                $req->respond([
+                    200,
+                    'OK',
+                    { 'Content-Type' => 'text/plain' },
+                    "pong\n",
+                ]);
+            }
+            else {
+                $req->respond([
+                    400,
+                    'Bad Request',
+                    { 'Content-Type' => 'text/plain' },
+                    "failed to pong\n",
+                ]);
+            }
+        },
+    );
+};
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=head1 SYNOPSIS
+
+    package Your::Module;
+    use Moo;
+    with qw(
+        OpenCloset::Cron::Role::HTTPD
+        OpenCloset::Cron::Role::Ping
+    );
+
+    sub BUILD {
+        my $self = shift;
+
+        $self->httpd->reg_cb(
+            '' => sub {
+                my ( $httpd, $req ) = @_;
+
+                $req->respond([ 404, 'not found', { 'Content-Type' => 'text/plain' }, "not found\n" ]);
+            },
+        );
+    }
+
+    package main;
+    use AnyEvent;
+    use Your::Module;
+
+    my $ym = Your::Module->new(
+        port => 20000,
+        ping => sub { return is_alive() ? 1 : 0; },
+    );
+
+    $ym->httpd->reg_cb(
+        '/foo' => sub {
+            my ( $httpd, $req ) = @_;
+
+            $req->respond([ 200, 'OK', { 'Content-Type' => 'text/plain' }, "foo\n" ]);
+        },
+        '/bar' => sub {
+            my ( $httpd, $req ) = @_;
+
+            $req->respond([ 200, 'OK', { 'Content-Type' => 'text/plain' }, "bar\n" ]);
+        },
+    );
+
+    AnyEvent->condvar->recv;
+
+    sub is_alive { return 1; }
+
+    # $ curl http://localhost:20000/ping
+    # pong
+
+
+=head1 DESCRIPTION
+
+This role will help to equip ping based on HTTP feature in you module.
+
+
+=attr ping
---
+++ lib/OpenCloset/Cron/Worker.pm
@@ -0,0 +1,122 @@
+package OpenCloset::Cron::Worker;
+# ABSTRACT: OpenCloset cron worker module
+
+use utf8;
+
+use Moo;
+use MooX::Types::MooseLike::Base qw( CodeRef Str );
+use namespace::clean -except => 'meta';
+
+our $VERSION = '0.001';
+
+use AnyEvent::Timer::Cron;
+use AnyEvent;
+use Scalar::Util qw( weaken );
+
+has name => ( is => 'ro', isa => Str,     required => 1 );
+has cron => ( is => 'ro', isa => Str,     required => 1 );
+has cb   => ( is => 'rw', isa => CodeRef, builder  => '_default_cb' );
+
+sub _default_cb {
+    my $self = shift;
+
+    weaken($self);
+    return sub {
+        my $name = $self->name;
+        my $cron = $self->cron;
+        AE::log( info => "$name\[$cron] dummy cron worker" );
+    };
+}
+
+has _timer => (
+    is        => 'rw',
+    predicate => '_has_timer',
+    clearer   => '_clear_timer',
+);
+
+has _cron => (
+    is        => 'rw',
+    predicate => '_has_cron',
+    clearer   => '_clear_cron',
+);
+
+sub register {
+    my $self = shift;
+
+    my $name = $self->name;
+    my $cron = $self->cron;
+    my $cb   = $self->cb;
+
+    $cron //= q{};
+    AE::log( debug => "$name: cron[$cron]" );
+
+    if ( !$cron || $cron =~ /^\s*$/ ) {
+        if ( $self->_has_timer ) {
+            AE::log( info  => "$name: clearing timer, cron rule is empty" );
+            $self->_clear_cron;
+            $self->_clear_timer;
+        }
+        return;
+    }
+
+    my @cron_items = split q{ }, $cron;
+    unless ( @cron_items == 5 ) {
+        AE::log( warn  => "$name: invalid cron format" );
+        return;
+    }
+
+    if ( $self->_has_timer ) {
+        AE::log( debug => "$name: timer is already exists" );
+
+        if ( $cron && $cron eq $self->_cron ) {
+            return;
+        }
+        AE::log( info => "$name: clearing timer before register" );
+        $self->_clear_cron;
+        $self->_clear_timer;
+    }
+
+    AE::log( info => "$name: register [$cron]" );
+    my $cron_timer = AnyEvent::Timer::Cron->new(
+        cron => $cron,
+        cb   => $cb,
+    );
+    $self->_cron($cron);
+    $self->_timer($cron_timer);
+}
+
+1;
+
+# COPYRIGHT
+
+__END__
+
+=head1 SYNOPSIS
+
+    use OpenCloset::Cron::Worker;
+
+    my $worker1 = do {
+        my $w; $w = OpenCloset::Cron::Worker->new(
+            name => '1min_cron',
+            cron => '* * * * *',
+            cb   => sub {
+                my $name = $w->name;
+                my $cron = $w->cron;
+                AE::log( info => "$name $cron 1 minute cron worker" );
+            },
+        );
+    };
+
+
+=head1 DESCRIPTION
+
+...
+
+
+=attr name
+
+=attr cron
+
+=attr cb
+
+=method register
